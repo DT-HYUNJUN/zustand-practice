d9a837806e4a53b9380c2c33c2ceaca1fb83c481
Park <angle307@naver.com>
2024-06-01 23:24:55 +0900

    feat: focus input field when pressing /

--- src/components/Home/Control.tsx
+++ src/components/Home/Control.tsx
@@ -2,12 +2,14 @@ import { Plus } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import useTodoStore from "@/store/store";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { convertToMMDD } from "@/utils/formatDate";
 import { useNavigate } from "react-router-dom";
 
 const Control = () => {
   const [newTodoText, setNewTodoText] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const navigate = useNavigate();
 
@@ -20,14 +22,30 @@ const Control = () => {
     setNewTodoText("");
   };
 
+  useEffect(() => {
+    const handleKeydownInput = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        e.preventDefault();
+        inputRef.current?.focus();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydownInput);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydownInput);
+    };
+  }, []);
+
   return (
     <form onSubmit={handleSubmit} className="flex gap-2 h-12">
       <Input
         type="text"
-        className="rounded-full h-full"
+        ref={inputRef}
+        className="rounded-full h-full pl-6"
         value={newTodoText}
         onChange={(e) => setNewTodoText(e.target.value)}
-        placeholder={`${typeof selectedDate === "object" && convertToMMDD(selectedDate, true)}에 일정 추가`}
+        placeholder={`/를 눌러 ${typeof selectedDate === "object" && convertToMMDD(selectedDate, true)}에 일정 추가`}
       />
       <div>
         <Button className="rounded-full shadow-xl w-12 h-12" variant="outline" size="icon">
